dafa5fc7abfe0998274d4451da54f36d45d9852c
guvanch-amangeldiyev-idf <guvanch.amangeldiyev@idfinance.com>
2022-03-04 11:23:29 +0300

    nw

---
+++ packages/presentation/lib/pages/login/with_stream/login.dart
@@ -0,0 +1,25 @@
+// import 'package:data/repository/login/login_impl.dart';
+// import 'package:flutter/material.dart';
+//
+// part 'login_part.dart';
+//
+// final RegistrationImpl bloc = RegistrationImpl();
+//
+// class LoginPage extends StatelessWidget {
+//   const LoginPage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: SafeArea(
+//       child: Column(
+//         children: [
+//           _EmailStream(register: bloc),
+//           _PasswordStream(register: bloc),
+//           const Spacer(),
+//           _ButtonStream(register: bloc),
+//         ],
+//       ),
+//     ));
+//   }
+// }
